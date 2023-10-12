a0ea650f91d8dc683645e599187ecc976b9917f6
Luiz Ricardo Guarezzi <lguarezzi@gmail.com>
2023-10-12 15:37:21 -0300

    criado componente de login do header

---
+++ src/app/govbr-components/govbr-header/header-login/header-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-header-login',
+  templateUrl: './header-login.component.html',
+  styleUrls: ['./header-login.component.css']
+})
+export class HeaderLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
