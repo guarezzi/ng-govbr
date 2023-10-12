22f2c1089ba616b34030466a229b587a2d68fc20
Luiz Ricardo Guarezzi <lguarezzi@gmail.com>
2023-10-12 15:05:19 -0300

    adicionado inicialização do componente header

--- src/app/govbr-components/govbr-header/header/header.component.ts
+++ src/app/govbr-components/govbr-header/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { GovBrComponentsEnum } from '../../shared/govbr-components.enum';
+import { GovbrService } from '../../shared/govbr.service';
+import { HeaderOptions } from './shared/header-options.model';
 
 @Component({
   selector: 'br-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
+
+  @Input()
+  options!: HeaderOptions;
+
+  @ViewChild('BRHeader')
+  template!: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    new GovbrService().initialize(GovBrComponentsEnum.BRHeader, this.template?.nativeElement);
   }
 
 }
