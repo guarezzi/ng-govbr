93d74763000c1d2ca97a304c0178ae5d59789f89
Luiz Ricardo Guarezzi <lguarezzi@gmail.com>
2023-10-12 15:37:56 -0300

    criado componente de functions do header

---
+++ src/app/govbr-components/govbr-header/header-functions/header-functions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-header-functions',
+  templateUrl: './header-functions.component.html',
+  styleUrls: ['./header-functions.component.css']
+})
+export class HeaderFunctionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
