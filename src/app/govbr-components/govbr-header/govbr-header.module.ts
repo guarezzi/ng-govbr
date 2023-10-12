1ef91dd69e47bed182088f8b402592d2df2ebfc4
Luiz Ricardo Guarezzi <lguarezzi@gmail.com>
2023-10-12 15:38:18 -0300

    Adicionado components de login, links e functions no header.

--- src/app/govbr-components/govbr-header/govbr-header.module.ts
+++ src/app/govbr-components/govbr-header/govbr-header.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { HeaderLinksComponent } from './header-links/header-links.component';
+import { HeaderFunctionsComponent } from './header-functions/header-functions.component';
+import { HeaderLoginComponent } from './header-login/header-login.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    HeaderLinksComponent,
+    HeaderFunctionsComponent,
+    HeaderLoginComponent
   ],
   exports: [
     HeaderComponent
